f9380f173e4b239a94aa01f6cfc19dab40c67593
Brightergy-Bruce <bruce.wang@brightergy.com>
2016-03-31 08:55:26 -0500

    second round

--- perf/electric_demand.ex
+++ perf/electric_demand.ex
@@ -1,40 +1,41 @@
-time_start = :os.system_time(:milli_seconds);
+defmodule Crap do
 
-defmodule Rand do
-	def get_random do
-		:random.seed(:os.timestamp)
-		:random.uniform * 100
-	end
+  def calc(num_points) do
+
+    time_start = :os.system_time(:milli_seconds);
+    collection = 1..num_points;
+
+    collection
+    |> Enum.map(&Task.async(fn -> &1 = get_random end))
+    |> Enum.map(&Task.await/1)
+    |> Enum.chunk(15 * 60, 1)
+    |> Enum.map(&Task.async(fn -> average(&1) end))
+    |> Enum.map(&Task.await/1)
+    |> IO.inspect
+    
+    time_end = :os.system_time(:milli_seconds);
+    IO.puts("Total time: ");
+    IO.puts(time_end - time_start);
+
+  end
+
+  def sum([head|tail], total) do
+      sum(tail, head + total)
+  end
+
+  def sum([], total) do
+      total
+  end
+
+  def average(list) do
+      len = length list;
+      sum(list, 0) / len;
+  end
+
+  def get_random do
+    :random.seed(:os.timestamp)
+    :random.uniform * 100
+  end
 end
 
-defmodule Summation do
-	def sum([head|tail], total) do
-		sum(tail, head + total)
-	end
-	
-	def sum([], total) do
-		total
-	end
-	
-	def average(list) do
-		len = length list;
-		sum(list, 0) / len;
-	end
-end
-
-num_points = 100000;
-fifteen_minutes = 15 * 60;
-
-all_points = Enum.map(1..num_points, fn x -> Rand.get_random end)
-
-chunks = Enum.chunk(all_points, fifteen_minutes, 1);
-
-
-calculated_points = Enum.map(chunks, fn x -> Summation.average(x) end);
-time_end = :os.system_time(:milli_seconds);
-
-IO.puts("Total elements:");
-IO.puts(length all_points);
-
-IO.puts("Total time: ");
-IO.puts(time_end - time_start);
+Crap.calc(100_000)
