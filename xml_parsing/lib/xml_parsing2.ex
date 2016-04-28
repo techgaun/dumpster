42cb11e9c680c381c2addb0e8842dfa8595d2e2b
Brightergy-JonathanHockman <jonathan.hockman@brightergy.com>
2016-04-28 00:17:43 -0500

    Xml testing (#1)
    * Update testing for retrieving xml from egauge. Not complete yet
    
    * Implement Digest auth header and basic parse of xml data
    
    * Parse xml from egauge and create list of values
    
    * Update xml_parsing test. Output ms time taken to create auth header and parse xml returned data. Parse meta_data, register name for Grid, and reading for register.
    
    * Just updating the test to use more of an Elixir type method
    
    * Add basic concurrency. Verify time stays the same as with a single request.
    
    * Add full concurrency
    
    * Add timer to get egauge data every 10 seconds
    
    * Implement use of Floki for xml parsing

---
+++ xml_parsing/lib/xml_parsing2.ex
@@ -0,0 +1,2 @@
+defmodule XmlParsing2 do
+end
--- xml_parsing/mix.exs
+++ xml_parsing/mix.exs
@@ -14,7 +14,7 @@ defmodule XmlParsing.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :httpoison]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,10 @@ defmodule XmlParsing.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:httpoison, "~> 0.8.0"},
+      {:exml, git: "https://github.com/expelledboy/exml.git", tag: "v0.1.0"},
+      {:floki, "~> 0.8"}
+    ]
   end
 end
