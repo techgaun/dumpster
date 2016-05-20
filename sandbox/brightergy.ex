111b0cf040ced9e502a6cf2992b0dc91a8942ee9
Brightergy-Bruce <bruce.wang@brightergy.com>
2016-05-20 14:41:50 -0500

    update methodology

--- sandbox/brightergy.ex
+++ sandbox/brightergy.ex
@@ -1,3 +1,4 @@
+
 defmodule Brightergy do
 
     def info(:languages), do: {"Elixir", "Python", "Javascript"}
@@ -5,8 +6,9 @@ defmodule Brightergy do
     def info(:services), do: {"Heroku", "Runscope", "Postgres", "Honeybadger", "Papertrail", "Intercom", "Auth0"}
     def info(:tools), do: {"GitHub", "Trello", "SemaphoreCI", "InVision", "Slack"}
     def info(:hardware), do: {"Schneider", "Digi", "Zigbee", "eGauge", "Webbox", "Fornius"}
+    def info(:methodologies), do: {"Continuous Delivery", "Design Thinking"}
     def info(:brighterlink) do
-        {:languages, :frameworks, :services, :tools, :hardware}
+        {:languages, :frameworks, :services, :tools, :hardware, :methodologies}
         |> Tuple.to_list
         |> Enum.map(fn(x) -> "#{x} = " <> (info(x) |> Tuple.to_list |> Enum.join(",")) end)
         |> Enum.join(" \n")
