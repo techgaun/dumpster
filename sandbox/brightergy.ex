51206bec84657c39d463ce2c75c7f1611028dc9c
Brightergy-Bruce <bruce.wang@brightergy.com>
2016-05-20 14:49:54 -0500

    update code to include guards

--- sandbox/brightergy.ex
+++ sandbox/brightergy.ex
@@ -7,13 +7,14 @@ defmodule Brightergy do
     def info(:tools), do: {"GitHub", "Trello", "SemaphoreCI", "InVision", "Slack"}
     def info(:hardware), do: {"Schneider", "Digi", "Zigbee", "eGauge", "Webbox", "Fornius"}
     def info(:methodologies), do: {"Continuous Delivery", "Design Thinking", "Mobile First"}
-    def info(:brighterlink) do
+    def info(:brighterlink = type) when is_atom(type) do
         {:languages, :frameworks, :services, :tools, :hardware, :methodologies}
         |> Tuple.to_list
         |> Enum.map(fn(x) -> "#{x} = " <> (info(x) |> Tuple.to_list |> Enum.join(",")) end)
-        |> Enum.join(" \n")
+        |> Enum.join("\n")
         |> IO.puts
     end
+    def info(type), do: "Sorry, we don't do that" |> IO.puts
 end
 
 Brightergy.info(:brighterlink)
