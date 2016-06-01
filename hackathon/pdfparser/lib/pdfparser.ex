41f3bc613b75f3af74b7f638d8a25c7d4638d21f
Brightergy-Bruce <bruce.wang@brightergy.com>
2016-05-31 23:06:16 -0500

    parse pdf files

--- hackathon/pdfparser/lib/pdfparser.ex
+++ hackathon/pdfparser/lib/pdfparser.ex
@@ -1,18 +1,25 @@
 defmodule Pdfparser do
   import Pdf2htmlex
 
-  def parse do
-
+  def parse(files) when is_list(files) do
+    Enum.map(files, fn(x) -> parse(x) end)
   end
 
-  def parse(file, out_file) do
-    file
+  def parse(file) do
+    out_file = String.replace_suffix(file, ".pdf", ".html")
+    IO.puts "converting #{file} to #{out_file}"
+    Path.join('pdfs', file)
     |> open 
     |> zoom(1.5)
     |> hdpi(96)
     |> vdpi(96)
-    |> convert_to!(out_file)
+    |> convert_to!(Path.join('html', out_file))
   end
 end
 
+
+["03440-96007.pdf",
+"33100-0116.pdf"]
+|>
 Pdfparser.parse
+
