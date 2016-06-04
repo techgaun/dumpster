6a3c23cce05a4333a17d2ae8a863f401c8fa6ba6
techgaun <coolsamar207@gmail.com>
2016-06-03 19:05:14 -0500

    minor updates

--- hackathon/utility-analytics/utility_analyzer/lib/utility_analyzer.ex
+++ hackathon/utility-analytics/utility_analyzer/lib/utility_analyzer.ex
@@ -14,15 +14,15 @@ defmodule UtilityAnalyzer do
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :simple_one_for_one, name: UtilityAnalyzer.Supervisor]
+    opts = [strategy: :simple_one_for_one, name: :utility_analyzer]
     {:ok, sup_pid} = Supervisor.start_link(children, opts)
     src_dir
     |> Path.join("*.pdf")
     |> Path.wildcard
     |> Enum.each(fn fname ->
       require Logger
-      Logger.warn inspect "Starting child for #{fname}"
-      Logger.warn inspect Supervisor.start_child(sup_pid, [[name: fname]])
+      Logger.debug inspect "Starting child for #{fname}"
+      Supervisor.start_child(sup_pid, [[name: fname]])
     end)
     {:ok, sup_pid}
   end
