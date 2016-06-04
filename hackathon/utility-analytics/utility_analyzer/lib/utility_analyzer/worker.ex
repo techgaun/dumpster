0c9064dbef6c8435cefdeff60d8284236ab0f6df
techgaun <coolsamar207@gmail.com>
2016-06-03 19:26:29 -0500

    fix issue with multi workers restart

--- hackathon/utility-analytics/utility_analyzer/lib/utility_analyzer/worker.ex
+++ hackathon/utility-analytics/utility_analyzer/lib/utility_analyzer/worker.ex
@@ -11,7 +11,7 @@ defmodule UtilityAnalyzer.Worker do
   end
 
   def shutdown(pdf_file) do
-    GenServer.stop(process_ident(pdf_file), :normal)
+    GenServer.stop(process_ident(pdf_file[:name]), :normal)
   end
 
   def init(pdf_file) do
@@ -22,12 +22,14 @@ defmodule UtilityAnalyzer.Worker do
   def handle_info(:extrparse, pdf_file) do
     # outfile = "#{tmp_dir}/#{random_string(20)}.txt"
     pdf_text = Processor.pdftotext(pdf_file[:name])
+    Logger.warn inspect pdf_text
     pdf_text_len = pdf_text |> byte_size
     if pdf_text_len < 100 do
       Logger.warn inspect "The pdf file #{pdf_file[:name]} is not a native pdf"
     else
       parsed_text = Ameren.parse(pdf_text)
     end
+    shutdown(pdf_file)
     {:stop, :normal, pdf_file}
   end
 
@@ -39,5 +41,5 @@ defmodule UtilityAnalyzer.Worker do
     :crypto.strong_rand_bytes(length) |> Base.url_encode64 |> binary_part(0, length)
   end
 
-  defp process_ident(str) when is_bitstring(str), do: String.replace(str, ~r/(\/|\s|-|\.)/, "_") |> String.strip(?_) |> String.to_atom
+  defp process_ident(str) when is_bitstring(str), do: str |> Path.basename(".pdf") |> String.replace(~r/(\/|\s|-|\.)/, "_") |> String.strip(?_) |> String.to_atom
 end
