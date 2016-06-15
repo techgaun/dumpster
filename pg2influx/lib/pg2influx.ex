9106db8fa901658428cfb3c62d8af0c30c97916c
techgaun <coolsamar207@gmail.com>
2016-06-15 02:19:25 -0500

    fix query issue

--- pg2influx/lib/pg2influx.ex
+++ pg2influx/lib/pg2influx.ex
@@ -1,16 +1,21 @@
 defmodule PG2Influx do
   use Application
   import BrighterlinkIo.DeviceDataDB
+  require Logger
 
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
     influx_worker = BrighterlinkIo.InfluxConnection.child_spec
     pg_worker = worker(BrighterlinkIo.DeviceDataDB, [Moebius.get_connection])
     {:ok, pid} = Supervisor.start_link [influx_worker, pg_worker], [strategy: :one_for_one, name: :pg2influx]
+    Logger.debug "Starting the process of postgres to influx migration"
     devices = read_devices |> Enum.join(", ")
-    count_query = "select count(id) as data_count from devices_data where id in (#{devices});"
+    Logger.warn inspect devices
+    count_query = "select count(id) as data_count from devices_data where device_id in (#{devices});"
+    Logger.warn inspect count_query
     [%{data_count: devices_data_count}] = count_query
       |> db_query
+    Logger.debug inspect "Total number of devices data is #{devices_data_count}"
     offset = if devices_data_count > 1000, do: devices_data_count - 1000, else: 0
     loop_query(devices, 1000, offset)
     {:ok, pid}
