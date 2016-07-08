482a5f2d6e5be4fc1f1e84f933bd70f16220626e
Brightergy-Bruce <bruce.wang@brightergy.com>
2016-07-08 14:41:11 -0500

    add new datastores

--- sandbox/brightergy.ex
+++ sandbox/brightergy.ex
@@ -2,12 +2,13 @@ defmodule Brightergy do
 
     def info(:languages), do: {"Elixir", "Python", "Javascript"}
     def info(:frameworks), do: {"Phoenix", "ReactJS"}
+    def info(:datastore), do: {"Postgres", "InfluxDB"}
     def info(:services), do: {"Heroku", "Runscope", "Postgres", "Honeybadger", "Papertrail", "Intercom", "Auth0"}
     def info(:tools), do: {"GitHub", "Trello", "SemaphoreCI", "InVision", "Slack"}
     def info(:hardware), do: {"Schneider", "Digi", "Zigbee", "eGauge", "Webbox", "Fornius"}
     def info(:methodologies), do: {"Continuous Delivery", "Design Thinking", "Mobile First"}
     def info(:brighterlink = type) when is_atom(type) do
-        {:languages, :frameworks, :services, :tools, :hardware, :methodologies}
+        {:languages, :frameworks, :datastore, :services, :tools, :hardware, :methodologies}
         |> Tuple.to_list
         |> Enum.map(fn(x) -> "#{x} = " <> (info(x) |> Tuple.to_list |> Enum.join(",")) end)
         |> Enum.join("\n")
